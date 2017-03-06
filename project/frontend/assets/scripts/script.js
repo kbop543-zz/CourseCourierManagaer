12e2e8299f1180d0a2efb2aa75b2070344005ee4
JeremyGralla <jeremy.gralla@mail.utoronto.ca>
2017-03-06 03:21:11 -0500

    Formatted new hat logo, added responsive UI, form sytling

--- project/frontend/assets/scripts/script.js
+++ project/frontend/assets/scripts/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $('#upload').click(function () {
         $("main").empty(); 
-        $("main").append('<form action="/actiononfile"> Course: <input type="file" name="Upload Here!"><br><input type="submit" value="Upload"><input type="reset" value="Reset"><br></form>');
+        $("main").append('<form action="/actiononfile"> Course: <input type="file" name="coursefile"><br><input type="submit" value="Upload"><input type="reset" value="Reset"><br></form>');
     });
     $('#login').click(function () {
         $("main").empty();
-        $("main").append('<form action="/actiononfile"> Username: <input type="text" name="Username"><br> Password: <input type="text" name="Username"><br><input type="submit" value="Log In"><input type="reset" value="Reset"><br></form>');
+        $("main").append('<form action="/actiononfile"> Username: <input type="text" name="Username"><br> Password: <input type="text" name="Password"><br><input type="submit" value="Log In"><input type="reset" value="Reset"><br></form>');
     });
     $('#about').click(function () {
         $("main").empty();
