75e8e42972d51c21dc9803ca780e3ccb50d0449d
Doovongeman <john.axon@yahoo.ca>
2017-03-07 14:17:33 -0500

    Updated the ExportedCalendarFile.java to parse json data. Included an example where you can create multiple events.

--- project/ExportedCalendarFile/src/ExportedCalendarFile.java
+++ project/ExportedCalendarFile/src/ExportedCalendarFile.java
@@ -1,47 +1,65 @@
+package algorithms;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.io.PrintWriter;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ExportedCalendarFile {
+	private String jsonData;
+	private List<ArrayList<String>> events = new ArrayList<ArrayList<String>>();
 	
-	public String BuildEvent(
-			String summary,
-			String startTime,
-			String startDate,
-			String endTime,
-			String endDate,
-			String description,
-			String location,
-			String exTime,
-			String exDate,
-			String trigger,
-			String repeat,
-			String duration,
-			String alarmDescription) {
-		String header = new StringBuilder()
+	public String jsonTimeStampToEventTimeStamp(String jsonTimeStamp) {
+		String eventTimeStamp = jsonTimeStamp.replace("-", "");
+		eventTimeStamp = eventTimeStamp.replace(":", "");
+		eventTimeStamp = eventTimeStamp.replace("Z", "");
+		return eventTimeStamp;
+	}
+	
+//	Acts as a header and a footer to the BuildEvent call
+	public String BuildEvents() {
+		//Header
+		String calendarFileString = new StringBuilder()
 				.append(String.format("BEGIN:VCALENDAR\n"))
 				.append(String.format("VERSION:2.0\n"))
 				.append(String.format("CALSCALE:GREGORIAN\n"))
 				.toString();
-		summary = summary == null ? "CSC301H1 LEC5101" : summary;
-		startTime = startTime == null ? "183000" : startTime;
-		startDate = startDate == null ? "20170309" : startDate;
-		String startDateAndTime = startDate + "T" + startTime;
-		endTime = endTime == null ? "203000" : endTime;
-		endDate = endDate == null ? "20170309" : endDate;
-		String endDateAndTime = endDate + "T" + endTime;
-		description = description == null ? 
-				"Introduction to Software\\nBAHEN CENTRE FOR INFORMATION TECH" : 
-					description;
-		location = location == null ? "BA 1200" : location;
-		exTime = exTime == null ? "180000" : exTime;
-		exDate = exDate == null ? "20170220" : exDate;
-		String exDateAndTime = exDate + "T" + exTime;
-		trigger = trigger == null ? "-PT1H" : trigger;
-		repeat = repeat == null ? "4" : repeat;
-		duration = duration == null ? "PT15M" : duration;
-		alarmDescription = alarmDescription == null ? 
-				"Introduction to Software" : alarmDescription;
+		String footer = "END:VCALENDAR";
+		
+		//Add Events
+		for(int i = 0; i < events.size(); ++i) {
+			String summary = events.get(i).get(0);
+			String description = events.get(i).get(1);
+			String startDateAndTime = jsonTimeStampToEventTimeStamp(events.get(i).get(2));
+			calendarFileString = calendarFileString + (BuildEvent(summary, startDateAndTime, description));
+		}
+		return calendarFileString + footer;
+	}
+	
+	
+	public String BuildEvent(
+			String summary,
+			String startDateAndTime,	
+			String description) {
+		String hour = startDateAndTime.substring(9, 11);
+		int duration = 1;
+		int hourInt = Integer.parseInt(hour);
+		int newHourInt = hourInt + duration;
+		String newHourString = Integer.toString(newHourInt);
+		String endDateAndTime = startDateAndTime.substring(0, 9) + newHourString + 
+				startDateAndTime.substring(11, startDateAndTime.length());
+//		String location = location == null ? "BA 1200" : location;
+		String location = "";
+		String exDateAndTime = startDateAndTime;
+		String trigger = "-PT1H";
+		String repeat = "4";
+		String alarmDuration = "PT15M";
+		String alarmDescription = summary;
 		String event = new StringBuilder()
 				.append(String.format("BEGIN:VEVENT\n"))
 				.append(String.format("SUMMARY:%s\n", summary))
@@ -53,77 +71,68 @@ public class ExportedCalendarFile {
 				.append(String.format("BEGIN:VALARM\n"))
 				.append(String.format("TRIGGER:%s\n", trigger))
 				.append(String.format("REPEAT:%s\n", repeat))
-				.append(String.format("DURATION:%s\n", duration))
+				.append(String.format("DURATION:%s\n", alarmDuration))
 				.append(String.format("DESCRIPTION:%s\n", alarmDescription))
 				.append(String.format("END:VALARM\n"))
 				.append(String.format("END:VEVENT\n"))
-				.append(String.format("END:VCALENDAR"))
 				.toString();
-		return header + event;
+		return event;
 	}
 	
-	public static void main(String [ ] args) throws FileNotFoundException {
-//		Initialize Variables
-		String summary;
-		String startTime;
-		String startDate;
-		String endTime;
-		String endDate;
-		String description;
-		String location;
-		String exTime;
-		String exDate;
-		String exDateAndTime;
-		String trigger;
-		String repeat;
-		String duration;
-		String alarmDescription;
+	public void readLinesInFile() throws IOException {
+		String filePath = "/Users/johnaxon/Documents/UofT/Winter 2017/"
+				+ "project-team-11/project/backend/src/main/resources/hypotheticalOutput.json";
+		jsonData = new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8); 
+    }
+	
+	public void parseJson() {
+		Pattern p = Pattern.compile("\"courseCode\":\"(\\w+)\"");
+		Matcher m = p.matcher(jsonData);
+		m.find();
+		System.out.println("Course " + m.group(1) + ".");
+		String courseCode = m.group(1);
+
+//		Get Event Names
+		p = Pattern.compile("\"name\":\\s*\"(\\w+)\"");
+		m = p.matcher(jsonData);
+		while (m.find()) {
+			System.out.println("Name " + m.group(1) + ".");
+			ArrayList<String> innerList = new ArrayList<String>();
+			innerList.add(courseCode + " " + m.group(1));
+			events.add(innerList);
+		}
+		
+//		Get Event Weights
+		p = Pattern.compile("\"weight\":\\s*\"(\\w+%)\"");
+		m = p.matcher(jsonData);
+		int index = 0;
+		while (m.find()) {
+			System.out.println("Weight " + m.group(1) + ".");
+			ArrayList<String> innerList = events.get(index);
+			innerList.add(m.group(1));
+			++index;
+		}
+		
+//		Get Event Due Dates
+		p = Pattern.compile("\"dueDate\":\\s*\"(\\d+-\\d+-\\d+T\\d+:\\d+:\\d+Z)\"");
+		m = p.matcher(jsonData);
+		index = 0;
+		while (m.find()) {
+			System.out.println("Due Dates " + m.group(1) + ".");
+			ArrayList<String> innerList = events.get(index);
+			innerList.add(m.group(1));
+			++index;
+		}
+    }
+	
+	public static void main(String [ ] args) throws FileNotFoundException, IOException {
 		ExportedCalendarFile file = new ExportedCalendarFile();
-//		Keybaord Input
-		Scanner keyboard = new Scanner(System.in);
-		System.out.println("Enter an event description:");
-		summary = keyboard.nextLine();
-		System.out.println("Enter an event start time in the format HHMMSS:");
-		startTime = keyboard.nextLine();
-		System.out.println("Enter an event start date in the format YYYYMMDD:");
-		startDate = keyboard.nextLine();
-		System.out.println("Enter an event end time in the format HHMMSS:");
-		endTime = keyboard.nextLine();
-		System.out.println("Enter an event end date in the format YYYYMMDD:");
-		endDate = keyboard.nextLine();
-		System.out.println("Enter an event description:");
-		description = keyboard.nextLine();
-		System.out.println("Enter an event location:");
-		location = keyboard.nextLine();
-		System.out.println("Enter an event exTime in the format HHMMSS:");
-		exTime = keyboard.nextLine();
-		System.out.println("Enter an event exDate in the format YYYYMMDD:");
-		exDate = keyboard.nextLine();
-		System.out.println("Enter an alarm trigger in the format -PT1H:");
-		trigger = keyboard.nextLine();
-		System.out.println("Enter a number of alarm repeats:");
-		repeat = keyboard.nextLine();
-		System.out.println("Enter the duration of each alarm in the format PT15M:");
-		duration = keyboard.nextLine();
-		System.out.println("Enter a description for the alarm:");
-		alarmDescription = keyboard.nextLine();
-		String eventToWrite = file.BuildEvent(
-				summary,
-				startTime,
-				startDate,
-				endTime,
-				endDate,
-				description,
-				location,
-				exTime,
-				exDate,
-				trigger,
-				repeat,
-				duration,
-				alarmDescription);
+		file.readLinesInFile();
+		file.parseJson();
+		String eventsToWrite = file.BuildEvents();
 //		File Output
-		try(PrintWriter out = new PrintWriter("FridayTest.ics")) {
-			out.println(eventToWrite);
+		try(PrintWriter out = new PrintWriter("TuesdayTest.ics")) {
+			out.println(eventsToWrite);
 		}
 	}
 }
