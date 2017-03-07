5b5386a5a951d1e58311b219693475588f585097
Doovongeman <john.axon@yahoo.ca>
2017-03-07 18:11:00 -0500

    Removed the serializer mentiosn from CalendarServlet.java.

--- project/backend/src/main/java/servlet/CalendarServlet.java
+++ project/backend/src/main/java/servlet/CalendarServlet.java
@@ -4,16 +4,13 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import backend.CalendarFileStringGenerator;
-import backend.CalendarFileStringSerializer;
 
 @WebServlet( name = "CalendarServlet", urlPatterns = { "/calendar" } )
 public class CalendarServlet extends HttpServlet
@@ -27,13 +24,11 @@ public class CalendarServlet extends HttpServlet
 	protected void doGet( final HttpServletRequest req, final HttpServletResponse resp ) throws ServletException, IOException
 	{
 		CalendarFileStringGenerator generator = new CalendarFileStringGenerator();
-		CalendarFileStringSerializer serializer = new CalendarFileStringSerializer();
-		String testString = generator.GenerateString();
-		serializer.serialize(testString);
+		String calendarString = generator.GenerateString();
 		final ServletOutputStream out = resp.getOutputStream();
 		String fileName = "Assignments.ics";		
-		InputStream fis = new ByteArrayInputStream(testString.getBytes(StandardCharsets.UTF_8));
-		resp.setContentLength((int) testString.length());
+		InputStream fis = new ByteArrayInputStream(calendarString.getBytes(StandardCharsets.UTF_8));
+		resp.setContentLength((int) calendarString.length());
 		resp.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
 		
 		ServletOutputStream os = resp.getOutputStream();
