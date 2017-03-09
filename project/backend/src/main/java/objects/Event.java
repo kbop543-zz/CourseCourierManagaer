f69e82648a4a60c888a9878fcd1b394072a0cefd
Hari Sharma <hari.sharma@mail.utoronto.ca>
2017-03-09 16:15:41 -0500

    Created an event object.

---
+++ project/backend/src/main/java/objects/Event.java
@@ -0,0 +1,30 @@
+package objects;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public interface Event {
+	public enum EventType {ASSIGNMENT, MIDTERM, EXAM}
+	
+	public Time getStartTime();
+	public Time getEndTime();
+	public int getDuration();
+	public String getLocation();
+	
+	public Date getAnnouncedDate();
+	public Date getDueDate();
+	
+	public int getWeight();
+	
+	public Time setStartTime();
+	public Time setEndTime();
+	public int setDuration();
+	public String setLocation();
+	
+	public Date setAnnouncedDate();
+	public Date setDueDate();
+	
+	public int setWeight();
+	
+	
+}
