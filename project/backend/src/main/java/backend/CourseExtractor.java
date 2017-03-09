f7810743d305a820b551ac0ca7399cda1a3efee1
Filip Fabiszak <f.fabiszak@gmail.com>
2017-03-09 00:19:03 -0500

    syllabus POST works, and parses object

--- project/backend/src/main/java/backend/CourseExtractor.java
+++ project/backend/src/main/java/backend/CourseExtractor.java
@@ -2,8 +2,6 @@ package backend;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -14,123 +12,161 @@ import objects.CourseImpl;
 import objects.Markable;
 import objects.MarkableImpl;
 
-public class CourseExtractor {
+public class CourseExtractor
+{
 	// I dont know what format it is in fo for now I will write it like this.
-	private BufferedReader s;
-	public CourseExtractor(BufferedReader s){
+	private final BufferedReader s;
+
+	public CourseExtractor( final BufferedReader s )
+	{
 		this.s = s;
 	}
 
-	private String getFile(){
-	StringBuilder sb = new StringBuilder();
-	String line;
-	try {
-		line = s.readLine();
-		while(line!=null){
-			sb.append(line+"\n");
+	private String getFile()
+	{
+
+		final StringBuilder sb = new StringBuilder();
+		String line;
+		try
+		{
 			line = s.readLine();
+			while( line != null )
+			{
+				sb.append( line + "\n" );
+				line = s.readLine();
+			}
 		}
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+		catch( final IOException e )
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return sb.toString();
+
 	}
-	
-	return sb.toString();
-	}
-	public Course run() throws Exception{
+
+	public Course run() throws Exception
+	{
 		// creating a new course
 		Course c = null;
 		boolean t = true;//  to need not code an if statement each check
-		
-		String file = getFile();
-		String line1 = file.split("\n")[0];
+
+		final String file = getFile();
+		final String line1 = file.split( "\n" )[0];
 		// getting the matcher object hopefully ecliplse imports pattern as well.
 		// 3 letters upper or lowecase followedby 3 digits to signify the course code
-		Matcher m  = Pattern.compile("([a-z]{3}|[A-Z]{3})\\d{3}").matcher(line1);
+		Matcher m = Pattern.compile( "([a-z]{3}|[A-Z]{3})\\d{3}" ).matcher( line1 );
 		t = m.find();
-		if(!t) throw new Exception();
-		String coursecode = line1.substring(m.start(), m.end());
-		
+		if( !t )
+		{
+			throw new Exception();
+		}
+		final String coursecode = line1.substring( m.start(), m.end() );
+
 		// I am considering the course name to be just the second line
-		String coursename = file.split("\n")[1];
+		final String coursename = file.split( "\n" )[1];
 		// creating the last final paramerter
-		List<Markable> lm = new ArrayList<Markable>();
-		m = Pattern.compile("[\\t \\n]*((A1|a1)|(A2|a2)|(A3|a3)|(Midterm|midterm)|(Exam|exam))[\\t \\n]+\\d{1,2}+%[\\t \\n]+20\\d{2}-(0[1-9]|1[0-2])-((0[1-9])|(1\\d)|(2\\d)|(30)|(31))[\\t \\n]+([0-1]\\d|2[0-4]):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d)):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d))[\\t \\n]*").matcher(file);
+		final List< Markable > lm = new ArrayList< Markable >();
+		m = Pattern
+			.compile( "[\\t \\n]*((A1|a1)|(A2|a2)|(A3|a3)|(Midterm|midterm)|(Exam|exam))[\\t \\n]+\\d{1,2}+%[\\t \\n]+20\\d{2}-(0[1-9]|1[0-2])-((0[1-9])|(1\\d)|(2\\d)|(30)|(31))[\\t \\n]+([0-1]\\d|2[0-4]):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d)):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d))[\\t \\n]*" )
+			.matcher( file );
 		t = m.find();
-		if(!t) throw new Exception();
-		String markable = file.substring(m.start(), m.end());
-		Matcher k = Pattern.compile("((A1|a1)|(A2|a2)|(A3|a3)|(Midterm|midterm)|(Exam|exam))").matcher(markable);
+		if( !t )
+		{
+			throw new Exception();
+		}
+		String markable = file.substring( m.start(), m.end() );
+		Matcher k = Pattern.compile( "((A1|a1)|(A2|a2)|(A3|a3)|(Midterm|midterm)|(Exam|exam))" ).matcher( markable );
 		k.find();
-		String mname = markable.substring(k.start(), k.end());
-		k = Pattern.compile("\\d{1,2}+%").matcher(markable);
+		String mname = markable.substring( k.start(), k.end() );
+		k = Pattern.compile( "\\d{1,2}+%" ).matcher( markable );
 		k.find();
-		String precentage = markable.substring(k.start(), k.end());
-		k = Pattern.compile("20\\d{2}-(0[1-9]|1[0-2])-((0[1-9])|(1\\d)|(2\\d)|(30)|(31))[\\t \\n]+([0-1]\\d|2[0-4]):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d)):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d))").matcher(markable);
+		String precentage = markable.substring( k.start(), k.end() );
+		k = Pattern
+			.compile( "20\\d{2}-(0[1-9]|1[0-2])-((0[1-9])|(1\\d)|(2\\d)|(30)|(31))[\\t \\n]+([0-1]\\d|2[0-4]):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d)):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d))" )
+			.matcher( markable );
 		k.find();
-		lm.add(new MarkableImpl(mname, precentage, markable.substring(k.start(),k.end())));
-		
+		lm.add( new MarkableImpl( mname, precentage, markable.substring( k.start(), k.end() ) ) );
+
 		// second markable. will demand at least 3 the rest are free to go.
 		t = m.find();
-		if(!t) throw new Exception();
-		 markable = file.substring(m.start(), m.end());
-		 k = Pattern.compile("((A1|a1)|(A2|a2)|(A3|a3)|(Midterm|midterm)|(Exam|exam))").matcher(markable);
+		if( !t )
+		{
+			throw new Exception();
+		}
+		markable = file.substring( m.start(), m.end() );
+		k = Pattern.compile( "((A1|a1)|(A2|a2)|(A3|a3)|(Midterm|midterm)|(Exam|exam))" ).matcher( markable );
 		k.find();
-		 mname = markable.substring(k.start(), k.end());
-		k = Pattern.compile("\\d{1,2}+%").matcher(markable);
+		mname = markable.substring( k.start(), k.end() );
+		k = Pattern.compile( "\\d{1,2}+%" ).matcher( markable );
 		k.find();
-		 precentage = markable.substring(k.start(), k.end());
-		k = Pattern.compile("20\\d{2}-(0[1-9]|1[0-2])-((0[1-9])|(1\\d)|(2\\d)|(30)|(31))[\\t \\n]+([0-1]\\d|2[0-4]):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d)):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d))").matcher(markable);
+		precentage = markable.substring( k.start(), k.end() );
+		k = Pattern
+			.compile( "20\\d{2}-(0[1-9]|1[0-2])-((0[1-9])|(1\\d)|(2\\d)|(30)|(31))[\\t \\n]+([0-1]\\d|2[0-4]):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d)):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d))" )
+			.matcher( markable );
 		k.find();
-		lm.add(new MarkableImpl(mname, precentage, markable.substring(k.start(),k.end())));
-		
-		
+		lm.add( new MarkableImpl( mname, precentage, markable.substring( k.start(), k.end() ) ) );
+
+
 		// third markable
-		
+
 		t = m.find();
-		if(!t) throw new Exception();
-		markable = file.substring(m.start(), m.end());
-		k = Pattern.compile("((A1|a1)|(A2|a2)|(A3|a3)|(Midterm|midterm)|(Exam|exam))").matcher(markable);
+		if( !t )
+		{
+			throw new Exception();
+		}
+		markable = file.substring( m.start(), m.end() );
+		k = Pattern.compile( "((A1|a1)|(A2|a2)|(A3|a3)|(Midterm|midterm)|(Exam|exam))" ).matcher( markable );
 		k.find();
-		mname = markable.substring(k.start(), k.end());
-		k = Pattern.compile("\\d{1,2}+%").matcher(markable);
+		mname = markable.substring( k.start(), k.end() );
+		k = Pattern.compile( "\\d{1,2}+%" ).matcher( markable );
 		k.find();
-		precentage = markable.substring(k.start(), k.end());
-		k = Pattern.compile("20\\d{2}-(0[1-9]|1[0-2])-((0[1-9])|(1\\d)|(2\\d)|(30)|(31))[\\t \\n]+([0-1]\\d|2[0-4]):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d)):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d))").matcher(markable);
+		precentage = markable.substring( k.start(), k.end() );
+		k = Pattern
+			.compile( "20\\d{2}-(0[1-9]|1[0-2])-((0[1-9])|(1\\d)|(2\\d)|(30)|(31))[\\t \\n]+([0-1]\\d|2[0-4]):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d)):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d))" )
+			.matcher( markable );
 		k.find();
-		lm.add(new MarkableImpl(mname, precentage, markable.substring(k.start(),k.end())));
-		
+		lm.add( new MarkableImpl( mname, precentage, markable.substring( k.start(), k.end() ) ) );
+
 		//fourth non madatory
 		t = m.find();
-		if(t){
-		 markable = file.substring(m.start(), m.end());
-		 k = Pattern.compile("((A1|a1)|(A2|a2)|(A3|a3)|(Midterm|midterm)|(Exam|exam))").matcher(markable);
-		 k.find();
-		 mname = markable.substring(k.start(), k.end());
-		k = Pattern.compile("\\d{1,2}+%").matcher(markable);
-		k.find();
-		 precentage = markable.substring(k.start(), k.end());
-		k = Pattern.compile("20\\d{2}-(0[1-9]|1[0-2])-((0[1-9])|(1\\d)|(2\\d)|(30)|(31))[\\t \\n]+([0-1]\\d|2[0-4]):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d)):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d))").matcher(markable);
-		k.find();
-		lm.add(new MarkableImpl(mname, precentage, markable.substring(k.start(),k.end())));
+		if( t )
+		{
+			markable = file.substring( m.start(), m.end() );
+			k = Pattern.compile( "((A1|a1)|(A2|a2)|(A3|a3)|(Midterm|midterm)|(Exam|exam))" ).matcher( markable );
+			k.find();
+			mname = markable.substring( k.start(), k.end() );
+			k = Pattern.compile( "\\d{1,2}+%" ).matcher( markable );
+			k.find();
+			precentage = markable.substring( k.start(), k.end() );
+			k = Pattern
+				.compile( "20\\d{2}-(0[1-9]|1[0-2])-((0[1-9])|(1\\d)|(2\\d)|(30)|(31))[\\t \\n]+([0-1]\\d|2[0-4]):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d)):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d))" )
+				.matcher( markable );
+			k.find();
+			lm.add( new MarkableImpl( mname, precentage, markable.substring( k.start(), k.end() ) ) );
 		}
-		
-		//fith non madatory
-				t = m.find();
-				if(t){
-				 markable = file.substring(m.start(), m.end());
-				 k = Pattern.compile("((A1|a1)|(A2|a2)|(A3|a3)|(Midterm|midterm)|(Exam|exam))").matcher(markable);
-				k.find();
-				 mname = markable.substring(k.start(), k.end());
-				k = Pattern.compile("\\d{1,2}+%").matcher(markable);
-				k.find();
-				 precentage = markable.substring(k.start(), k.end());
-				k = Pattern.compile("20\\d{2}-(0[1-9]|1[0-2])-((0[1-9])|(1\\d)|(2\\d)|(30)|(31))[\\t \\n]+([0-1]\\d|2[0-4]):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d)):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d))").matcher(markable);
-				k.find();
-				lm.add(new MarkableImpl(mname, precentage, markable.substring(k.start(),k.end())));
-				}
-				
 
-		c = new CourseImpl(coursecode,coursename,lm);
+		//fith non madatory
+		t = m.find();
+		if( t )
+		{
+			markable = file.substring( m.start(), m.end() );
+			k = Pattern.compile( "((A1|a1)|(A2|a2)|(A3|a3)|(Midterm|midterm)|(Exam|exam))" ).matcher( markable );
+			k.find();
+			mname = markable.substring( k.start(), k.end() );
+			k = Pattern.compile( "\\d{1,2}+%" ).matcher( markable );
+			k.find();
+			precentage = markable.substring( k.start(), k.end() );
+			k = Pattern
+				.compile( "20\\d{2}-(0[1-9]|1[0-2])-((0[1-9])|(1\\d)|(2\\d)|(30)|(31))[\\t \\n]+([0-1]\\d|2[0-4]):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d)):((0\\d)|(1\\d)|(2\\d)|(3\\d)|(4\\d)|(5\\d))" )
+				.matcher( markable );
+			k.find();
+			lm.add( new MarkableImpl( mname, precentage, markable.substring( k.start(), k.end() ) ) );
+		}
+
+
+		c = new CourseImpl( coursecode, coursename, lm );
 		return c;
 	}
 }
--- project/backend/src/main/java/servlet/SyllabusUploadServlet.java
+++ project/backend/src/main/java/servlet/SyllabusUploadServlet.java
@@ -13,6 +13,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
+import backend.CourseExtractor;
+import objects.Course;
+
 @WebServlet( name = "SyllabusUploadServlet", urlPatterns = { "/uploadSyllabus" } )
 @MultipartConfig
 public class SyllabusUploadServlet extends HttpServlet
@@ -26,16 +29,45 @@ public class SyllabusUploadServlet extends HttpServlet
 	@Override
 	protected void doPost( final HttpServletRequest req, final HttpServletResponse resp ) throws ServletException, IOException
 	{
-		//		final String description = req.getParameter( "description" ); // Retrieves <input type="text" name="description">
-
 		final Part filePart = req.getPart( "syllabus" ); // Retrieves <input type="file" name="file">
-
-		//		final String fileName = Paths.get( filePart.getSubmittedFileName() ).getFileName().toString(); // MSIE fix.
-
+		//		final String fileName = Paths.get( filePart.getSubmittedFileName() ).getFileName().toString(); // If we ever want to reference the original filename
 		final InputStream inputStream = filePart.getInputStream();
 		final BufferedReader fileContents = new BufferedReader( new InputStreamReader( inputStream ) );
+		final CourseExtractor syllabusExtractor = new CourseExtractor( fileContents );
 
-		System.out.println( fileContents.readLine() );
+		try
+		{
+			final Course courseFromSyllabus = syllabusExtractor.run();
+
+			System.out.println( "" );
+			System.out.println( "HERE IS EVERYTHING, BUT PARSED AND INTO JAVA OBJECTS!" );
+			System.out.println( "" );
+
+			System.out.println( courseFromSyllabus.getCourseCode() );
+			System.out.println( courseFromSyllabus.getCourseDescription() );
+			System.out.println( courseFromSyllabus.getMarkables().get( 0 ).getMarkableName() );
+			System.out.println( courseFromSyllabus.getMarkables().get( 0 ).getWeight() );
+			System.out.println( courseFromSyllabus.getMarkables().get( 0 ).getDueDate() );
+			System.out.println( courseFromSyllabus.getMarkables().get( 1 ).getMarkableName() );
+			System.out.println( courseFromSyllabus.getMarkables().get( 1 ).getWeight() );
+			System.out.println( courseFromSyllabus.getMarkables().get( 1 ).getDueDate() );
+			System.out.println( courseFromSyllabus.getMarkables().get( 2 ).getMarkableName() );
+			System.out.println( courseFromSyllabus.getMarkables().get( 2 ).getWeight() );
+			System.out.println( courseFromSyllabus.getMarkables().get( 2 ).getDueDate() );
+			System.out.println( courseFromSyllabus.getMarkables().get( 3 ).getMarkableName() );
+			System.out.println( courseFromSyllabus.getMarkables().get( 3 ).getWeight() );
+			System.out.println( courseFromSyllabus.getMarkables().get( 3 ).getDueDate() );
+			System.out.println( courseFromSyllabus.getMarkables().get( 4 ).getMarkableName() );
+			System.out.println( courseFromSyllabus.getMarkables().get( 4 ).getWeight() );
+			System.out.println( courseFromSyllabus.getMarkables().get( 4 ).getDueDate() );
+
+			System.out.println( "" );
+		}
+		catch( final Exception e )
+		{
+			System.out.println( "FAILED TO PARSE SYLLABUS" );
+			e.printStackTrace();
+		}
 
 	}
 
