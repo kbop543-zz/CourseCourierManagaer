752e9f1a36a29672157172a25299a95c4644a74f
JeremyGralla <jeremy.gralla@mail.utoronto.ca>
2017-03-23 03:26:51 -0400

    Added support for showing JSON

--- project/frontend/assets/scripts/courses.js
+++ project/frontend/assets/scripts/courses.js
@@ -9,7 +9,17 @@ function loadCourses (){
     })
     .done(function( data ) {
         $("main").empty();
-        $("main").append("<p>Here be the json<p/>" + JSON.stringify(data));
+			var obj = JSON.parse(JSON.stringify(data));
+			var course = obj.courses[0][0];
+			var mark = course.markables;
+        $("main").append('<p id = "courselist">' + course.courseCode + ": " + course.courseName + '</p>');
+		for (var i in mark){
+			$("p#courselist").append('<ul id = "mark"><li>' +
+			"Name: " + mark[i].name + "</li><li>" +
+			"Description: " + mark[i].description + "</li><li>" +
+			"Weight: " + mark[i].weight + "</li><li>" +
+			"Due Date: " + mark[i].dueDate + "</li></ul>");
+		}
     });
 }
 
