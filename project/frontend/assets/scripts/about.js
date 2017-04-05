f6f1621e35415930f307d4d3e84e6e737450adba
Hari Sharma <hari.sharma@mail.utoronto.ca>
2017-04-05 17:49:58 -0400

    Created about.js and about.html files.

---
+++ project/frontend/assets/scripts/about.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+	$('#courses').show();
+    $('#myMarks').show();
+    $('#profile').show();
+    $('#logout').show();
+    
+    $("main").append('<div id=profileHeading>Courier Course Manager</div>');
+    $("main").append('<div id=profileBody><br>Courier Course Manager was developed with one goal in mind - to make sure every student ' +
+    	'<br> can organize assignments </div>');
+    $("main").append('<div id=profileHeading>Courier Course Manager</div>');
+    $("main").append('<div id=profileHeading>Courier Course Manager</div>');
+    $("main").append('<div id=profileHeading>Courier Course Manager</div>');
+    $("main").append('<div id=profileHeading>Courier Course Manager</div>');
+    $("main").append('<div id=profileHeading>Courier Course Manager</div>');
+    $("main").append('<div id=profileHeading>Courier Course Manager</div>');
+    $("main").append('<div id=profileHeading>Courier Course Manager</div>');
+
+})
