60b869d478ae64d18446b894595c711e50905429
Hari Sharma <hari.sharma@mail.utoronto.ca>
2017-04-05 17:44:02 -0400

    Implemented profile.js formatting and profile display.

--- project/frontend/assets/scripts/profile.js
+++ project/frontend/assets/scripts/profile.js
@@ -10,18 +10,51 @@ function signupSetup (){
 		//console.log(data);
 		//dataCoursesParse(data);
         $("main").empty();
-        $("main").append("<div id=profileCard>"+'Welcome      '+"<form id='profileChange'><input type='text' name='firstName'  value="+data.firstName+"><input type='text' name='lastName'  value="+data.lastName+">" +
-						 "<br>"+'UserName :'+data.username +
-						 "<br>" +'Email :' +"<input type='text' name='email'  value="+data.email+">" +
-						 "<br>" +'Courses you are taking :' +	dataCoursesParse(data) +
-						 ' Your projected GPA is :' + gradeCalculator(data)+
-						 "<br><input type='submit' value='make changes'><br></form></div>" );
+       //  $("main").append("<div id=profileCard>"+'Welcome      '+"<form id='profileChange'><input type='text' name='firstName'  value="+data.firstName+"><input type='text' name='lastName'  value="+data.lastName+">" +
+						 // "<br>"+'UserName :'+data.username +
+						 // "<br>" +'Email :' +"<input type='text' name='email'  value="+data.email+">" +
+						 // "<br>" +'Courses you are taking :' +	dataCoursesParse(data) +
+						 // ' Your projected GPA is :' + gradeCalculator(data)+
+						 // "<br><input type='submit' value='make changes'><br></form></div>" );
 		
+		$("main").append('<div id=profileHeading>Welcome to Courier Course Manager.</div>');
+
+		$("main").append('<div id=profileSubHeading><br>Here is you profile info:</div>');
+		$("main").append('<div id=profileBody><br>First Name: ' + data.firstName + '</div>');
+		$("main").append('<div id=profileBody>Last Name: ' + data.lastName + '</div>');
+		$("main").append('<div id=profileBody>UserName: ' + data.username + '</div>');
+		$("main").append('<div id=profileBody>Email: ' + data.email + '</div>');
+
+		$("main").append('<div id=profileSubHeading><br>Courses you are taking:</div>');
+		$("main").append('<div id=profileBody>' + dataCoursesParse(data) + '</div>');
+
+		$("main").append('<div id=profileSubHeading><br>Your Projected GPA:</div>');
+		$("main").append('<div id=profileBody class="gpaValue"> ' + gradeCalculator(data) + '</div>');
+
+		$("main").append('<div id=profileSubHeading><br>Edit your profile info here:</div>');
+		$("main").append("<form id='profileChange'><br>" + 
+								"<label for='fname'>First Name</label><br><input type='text' id='fname' name='firstName' placeholder='New first name..'>" +
+								"<br><label for='lname'>Last Name</label><br><input type='text' id='lname' name='lastName' placeholder='New last name..'>" +
+								"<br><label for='lname'>Email</label><br><input type='text' id='email' name='email' placeholder='New email address..'>" +
+								"<br><input type='submit' value='Modify'>" +
+								"</form>");
+
+
+		     // '+"<form id='profileChange'><input type='text' name='firstName'  value="+data.firstName+"><input type='text' name='lastName'  value="+data.lastName+">" +
+						 // "<br>"+'UserName :'+data.username +
+						 // "<br>" +'Email :' +"<input type='text' name='email'  value="+data.email+">" +
+						 // "<br>" +'Courses you are taking :' +	dataCoursesParse(data) +
+						 // ' Your projected GPA is :' + gradeCalculator(data)+
+						 // "<br><input type='submit' value='make changes'><br></form></div>" );
 /*
         $('#username').val(data.username);
         $('#firstName').val(data.firstName);
         $('#lastName').val(data.lastName);
         $('#email').val(data.email);*/
+
+        $('#changeGrade').submit(function(event){
+        	('.gpaValue').text(gradeCalculator);
+        })
 
 		$('#profileChange').submit(function(event) {
 		console.log("this is called prime");
@@ -51,29 +84,37 @@ function signupSetup (){
 
 function dataCoursesParse(data){
 	var ajson_object;
-	try{ ajson_object= JSON.parse(data.courseObj);
-	   }catch(e){
+
+	try { 
+		ajson_object= JSON.parse(data.courseObj);
+	} catch(e) {
 		ajson_object = data.courseObj;	
-		}
-	//console.log(json_obj);
-	if(ajson_object != null){
-	var courses = ajson_object.courses;
-	var toReturn = "<br>" 
-	for(var i in courses){
-		//console.log(courses[i].courseCode);
-		if(courses[i].grade == null){
-			toReturn = toReturn + ' ' +courses[i].courseCode +"<input type='text' name='grade" +i+"'  value='Grade: Not available'><br>";
-		}
-		else{
-			toReturn = toReturn + ' ' +courses[i].courseCode +'      Grade: '+"<input type='text' name='grade" +i+"' value="+courses[i].grade+"><br>";
-		}
 	}
+
+	// console.log(ajson_object);
+
+	if(ajson_object != null){
+		var courses = ajson_object.courses;
+		console.log(courses);
+		var toReturn = "<form id='changeGrade'><br>"
+
+		for(var i in courses){
+			// console.log(courses[i].courseCode);
+			if(courses[i].grade == null){
+				toReturn += ' ' +courses[i].courseCode +"<input type='text' name='grade" +i+"'  value='Grade: Not available'><br>";
+			}
+			else{
+				toReturn += ' ' +courses[i].courseCode +'      Grade: '+"<input type='text' name='grade" +i+"' value="+courses[i].grade+"><br>";
+			}
+		}
+		toReturn +="<br><input type='submit' value='Test GPA output here'>" +"</form>";
 	
-	return toReturn;}
-	else{
-		return "<br>"+'None'+"<br>";;
+		return toReturn;
+	} else {
+		return '<br>' + 'You are not taking any courses right now.'+"<br>";;
 	}
 }
+
 function gradeCalculator(data){
 	var ajson_object;
 	try{ ajson_object= JSON.parse(data.courseObj);
@@ -83,25 +124,30 @@ function gradeCalculator(data){
 	if(ajson_object != null){
 	var courses = ajson_object.courses;
 	var count = 0;
-	var toReturn = 0
+	var toReturn = 0;
 	for(var i in courses){
 		console.log(courses[i].courseCode);
-		if(!(courses[i].grade == null)){
-			toReturn = toReturn + parseGrade(courses[i].grade);
-			count = count + 1;
+		console.log(courses[i].grade)
+		if(courses[i].grade != null){
+			
+			toReturn += courses[i].grade;
+			console.log(toReturn);
+			count++;
 		}
 	}
+	console.log(toReturn);
 	console.log(count);
 	if(count == 0) return 'Not Available';
-	return toReturn/count;
+	console.log(toReturn/count);
+	return parseGrade(toReturn/count);
 	}else{
-	return "<br>"+'None'+"<br>";
+	return "<br>"+'No GPA available.'+"<br>";
 	}
 }
 
 function parseGrade(grade){
 	var precentGrade = parseInt(grade);
-	var regularGrade;
+	var regularGrade = 0;
 	if(precentGrade > 49) regularGrade = 0.7;
 	if(precentGrade > 52) regularGrade = 1.0;
 	if(precentGrade > 56) regularGrade = 1.3;
